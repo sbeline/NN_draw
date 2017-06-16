99487382e8bf3c60c64bbaf3af6288f16f11e820
Samuel BELINE <sbeline@e1r7p18.42.fr>
2017-06-16 15:17:50 +0200

    re

--- coursera-week4.py
+++ coursera-week4.py
@@ -4,21 +4,22 @@ import scipy.io as sio
 class NeuronNetwork(object):
 	content = sio.loadmat('ex3data1.mat')
 	weights = sio.loadmat('ex3weights.mat')
-	print(content['X'].shape)
-	print(weights['Theta1'].shape)
-	print(weights['Theta2'].shape)
-	print(weights['Theta1'][0])
-	X = content['X']
+	X = np.matrix(content['X'])
 	y = content['y']
+	X = np.concatenate((np.ones((X[:,1].size, 1)), X) , axis=1)
 	def __init__(self):
 		self.InputLayer = len(X[0])
 		self.OutputLayer = 10
-		self.HidenLayer = 25
-	def propagate(self):
-		pass
-
-def sigmoid(z):
-	return 1 / (1 + np.exp(-z))
+		self.HidenLayer = 26
+		self.Theta1 = np.matrix(weights['Theta1'])
+		self.Theta2 = np.matrix(weights['Theta2'])
+	def Propagate(self, X):
+		z = sigmoid(self.Theta1 * X.T)
+		return (z)
+	def sigmoid(z):
+		return 1 / (1 + np.exp(-z))
 
 NN = NeuronNetwork()
-print(NN.InputLayer)
+print(NN.X.shape)
+a2 = NN.Propagate(NN.X)
+print(a2.shape)
